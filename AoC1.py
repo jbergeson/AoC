8c35f90f8e5e592d49a9996b6e24c95589a927f5
Jennifer Bergeson <jberges@purdue.edu>
2019-01-14 17:24:48 -0500

    Partial Soln to Part 2

--- AoC1.py
+++ AoC1.py
@@ -1,4 +1,32 @@
 import numpy as np
-data = np.loadtxt("D1P1.txt")
+#data = np.loadtxt("D1P1.txt")
+data = [1, 2, 3, 4, -4]
+myLength = len(data)
+zeroBase = myLength - 1
+#print("Length is %d" % zeroBase)
 ans = sum(data)
-print(ans)
+#print(ans)
+newFreq = np.cumsum(data)
+print(newFreq)
+def main():
+    a = 0
+    b = 1
+    var = False
+    while (var == False):
+        print("I ran")
+        while (b <= zeroBase and newFreq[a] != newFreq[b]):
+            print(a, b)
+            print(newFreq[a], newFreq[b])
+            b += 1
+            if (a == b):
+                b += 1
+        if (a == zeroBase):
+            var = True
+        elif b <=(zeroBase):
+            var = True
+            print("a is %d and b is %d" % (a, b))
+            print("Freq at a: %d, freq at b: %d" % (newFreq[a], newFreq[b]))
+        a += 1       
+        b = 0
+    return(a, b)
+main()
