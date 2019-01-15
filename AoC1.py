74480389449f600b0666f3007005acf6a81ebeb5
Jennifer Bergeson <jberges@purdue.edu>
2019-01-15 09:50:52 -0500

    Second Version

--- AoC1.py
+++ AoC1.py
@@ -1,32 +1,28 @@
 import numpy as np
-#data = np.loadtxt("D1P1.txt")
-data = [1, 2, 3, 4, -4]
-myLength = len(data)
-zeroBase = myLength - 1
-#print("Length is %d" % zeroBase)
-ans = sum(data)
-#print(ans)
-newFreq = np.cumsum(data)
-print(newFreq)
-def main():
-    a = 0
-    b = 1
-    var = False
-    while (var == False):
-        print("I ran")
-        while (b <= zeroBase and newFreq[a] != newFreq[b]):
-            print(a, b)
-            print(newFreq[a], newFreq[b])
-            b += 1
-            if (a == b):
-                b += 1
-        if (a == zeroBase):
-            var = True
-        elif b <=(zeroBase):
-            var = True
-            print("a is %d and b is %d" % (a, b))
-            print("Freq at a: %d, freq at b: %d" % (newFreq[a], newFreq[b]))
-        a += 1       
-        b = 0
-    return(a, b)
-main()
+import matplotlib.pyplot as plt
+import itertools 
+data = np.genfromtxt("D1P1.txt", dtype = int)
+## Practice data sets:
+#data = np.genfromtxt("PracData.txt", dtype = int)
+#data = [7, 7, -2, -7, -4]
+#data = [1, -2, 3, 4, 5, 6, -8, -2, 3, 4, 1, -9, -1, 8, -5]
+n = 5  # number of times to repeat data set
+data = list(data)
+data = n * data
+def getSums(data):
+    newFreq = list(itertools.accumulate(data))
+    myLength = len(data)
+    zeroBase = myLength - 1
+    last = newFreq[zeroBase]
+    print("The last is: %d" % last)
+    return(newFreq)
+
+def getAns(data):
+    newFreq = getSums(data)
+    myList = []
+    for x in newFreq:
+        if x in myList: break
+        else:
+            myList.append(x)
+    print("The final answer is: %d" %x)
+getAns(data)
